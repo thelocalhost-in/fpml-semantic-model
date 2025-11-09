e41910092b22d5971b3b600cab992cb395188844
uttesh <uttesh@gmail.com>
2025-11-09 12:48:52 +0530

    updated to 2.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setup(
     name='fpml-semantic-model',
-    version='v0.2.0',
+    version='v0.2.1',
     author='thelocalhost-in',
     author_email='thelocalhost.in@gmail.com',
     description='A semantic search and structural analysis model for FpML XSD schemas.',
